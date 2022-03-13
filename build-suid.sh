4c11ca656eaccbafb29b7add482cbd992359235e
lulz <oxfemale@users.noreply.github.com>
2022-03-13 05:43:50 +0300

    Update shell output help
    Update shell output help

--- build-suid.sh
+++ build-suid.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-echo 'build'
+echo "dummy hoto compile code:";
+echo "for compile C code on native linux need INSTALL developers tools:";
+echo "sudo apt update";
+echo "sudo apt install build-essential";
+echo "cc -o suid suid.c";
+echo "chmod u+s suid";
+echo "ls -al -G suid;echo \"bye bye security;)\""
+
+echo "Prepare build code.. ok Start:"
 cc -o suid suid.c
-echo 'set suid'
+echo "Setting sticky bit on file: set suid"
 chmod u+s suid
 ls -al suid
